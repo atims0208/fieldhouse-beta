fa6c9e7e39aeb086b21bd24f76f2abb8ccd65ec9
atims0208 <121659861+atims0208@users.noreply.github.com>
2025-04-16 11:57:41 -0500

    Fix configuration files

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true,
     images: {
         domains: ['vz-4f8c314d-49b.b-cdn.net'], // Add your Bunny.net domain
     },
     env: {
         NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    },
+    }
 }
 
 module.exports = nextConfig
